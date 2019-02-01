998aac0e214eb53c3c48d4a18db4fcc35348b52d
shirafuta <shirafuta.kodai@gmail.com>
2019-02-01 13:09:51 +0900

    update default config

--- config/adminbar.php
+++ config/adminbar.php
@@ -1,18 +1,52 @@
 <?php
+
 return array(
+
+    /**
+     *  
+     * Change this to false to disable admin bar
+     * 
+     * */
     'enabled'=>true,
-    //this applies to all routes. if you want to exclude specific route, please use filter.
+
+    /**
+     * 
+     * Please specify your admin pages' url so that Admin Bar does not show up in 
+     * your admin pages. 
+     * 
+     * The default is 'admin/*'
+     * When loading Admin Bar, It checks if current url matches the path set here with Illuminate\Http\Request::is().
+     *  
+     * */
     'excludes' => 'admin/*',
+
+    /**
+     * 
+     * In order to show Admin Bar only for logged in admin users,
+     * please specify how to tell if current visitor is logged in and also an admin 
+     * user.
+     * 
+     * As a default, we just return true.
+     * 
+     * */
     'is_admin' => function(){
+
+       // This is an example code. 
+       // 
+       // if( Auth::user() &&  Auth::user()->isAdmin()){
+       //     return true;
+       // }
+       // return false;
+        
         return true;
     },
+    /**
+     * 
+     * Specify links to show on Admin Bar.
+     * 
+     * */
     'menus' => array(
-        ['title'=>'Admin Top', 'path'=>'/path/to/admin/top'],
-        ['title'=>'Edit Post', 'path'=>'/edit/post_id'],
-        ['title'=>'Sub Menu', 'path'=>[
-                             ['title'=>'Option1', 'path'=>'/path/to/option1'],
-                             ['title'=>'Option2', 'path'=>'/path/to/option2']
-                            ]
-        ]
+        ['title'=>'Admin Top', 'path'=>'/admin'],
+        ['title'=>'Add a post', 'path'=>'/admin/post/new'],
         ),
 );
